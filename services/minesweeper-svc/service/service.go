afe6f41633f9a36e0460d48723c21d74437eb948
qiv1ne <sergeivanov827@gmail.com>
2025-01-29 13:01:33 +0300

    change game structure, need to write redis service before

--- services/minesweeper-svc/service/service.go
+++ services/minesweeper-svc/service/service.go
@@ -3,27 +3,52 @@ package service
 
 import (
 	"log/slog"
+	"time"
+
+	"github.com/google/uuid"
 
 	"github.com/qiv1ne/minesweeper"
 )
 
 // GameService interface represent main service
 type MinesweeperService interface {
+	NewMineBoard(config minesweeper.BoardConfig) (minesweeper.MineBoard, error)
 	OpenCell(x, y int) (int, error)
 	PlaceFlag(x, y int) (int, error)
 	NewSeed() int64
 }
 
-type minesweeperService struct {
-	Board *minesweeper.MineBoard
+type minesweeperService struct{}
+
+type GameSession struct {
+	BoardId   int                   `json:"board_id"`
+	Board     minesweeper.MineBoard `json:"board"`
+	CreatedAt time.Time             `json:"created_at"`
 }
 
-func NewGameService(config minesweeper.BoardConfig) (MinesweeperService, error) {
+func NewGameService(config minesweeper.BoardConfig) MinesweeperService {
 	slog.Info("creating new game service")
+	return minesweeperService{}
+}
+
+func (svc minesweeperService) NewGame(config minesweeper.BoardConfig) (minesweeper.MineBoard, error) {
+	// creating new board
 	board, err := minesweeper.NewMineBoard(config)
-	return minesweeperService{
-		Board: board,
-	}, err
+
+	// generating id for this game session
+	id := uuid.NewString()
+
+	// define when game session is created
+	t := time.Now().Format(time.TimeOnly)
+
+	// create session struct
+	session := GameSession{
+		BoardId:   id,
+		Board:     *board,
+		CreatedAt: t,
+	}
+
+	return *board, err
 }
 
 func (svc minesweeperService) OpenCell(x, y int) (int, error) {
